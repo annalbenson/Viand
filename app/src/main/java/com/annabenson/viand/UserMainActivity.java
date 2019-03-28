c5d861a8d4e69c31e84c649e1ab9799668519721
annalbenson <annalucybenson@gmail.com>
2019-03-27 19:56:56 -0500

    progress

--- app/src/main/java/com/annabenson/viand/UserMainActivity.java
+++ app/src/main/java/com/annabenson/viand/UserMainActivity.java
@@ -4,11 +4,27 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserMainActivity extends AppCompatActivity {
 
     private static final String TAG = "UserMainActivity";
+    private UserMainActivity mainActivity = this;
+
+    // views
+    private RecyclerView recyclerView;
+    private TextView locationView;
+
+    // vars
+    private StoreAdapter storeAdapter;
+    private List<Store> storeList = new ArrayList<>();
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
